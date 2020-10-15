93f03a03cf92d07221c52c62c413e981f9aa35ce
James Henderson <jhende1995@gmail.com>
2020-10-15 19:47:19 -0400

    Complete through indexOf

--- src/underbar.js
+++ src/underbar.js
@@ -48,11 +48,11 @@
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
     if ( Array.isArray(collection) ) {
-      for(var i = 0; i < collection.length; i++) {
+      for (var i = 0; i < collection.length; i++) {
         iterator( collection[i], i, collection);
       }
     } else if ( typeof collection === 'object' ) {
-      for(var key in collection) {
+      for (var key in collection) {
         iterator( collection[key], key, collection);
       }
     }
